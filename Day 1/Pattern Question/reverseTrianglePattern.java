57f9d873f88ef9629a4868ea3caf2d74c0832a97
Abhishek_Kumar <abhishekkumar958830@gmail.com>
2023-11-01 18:15:00 +0530

    Pattern question completed

--- Day 1/Pattern Question/reverseTrianglePattern.java
+++ Day 1/Pattern Question/reverseTrianglePattern.java
@@ -1,14 +1,13 @@
 
-public class charTrianglePattern {
+public class reverseTrianglePattern {
     public static void main(String[] args){
         int n = 5;
-        char a = 'a';
+
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <=i; j++){
-                System.out.print(" " + a);
+            for(int j = n; j >= i; j--){
+                System.out.print(" *");
             }
             System.out.println();
-            a++;
         }
     }
 }
